4e8479246ad96059566d1d87d7082dd15dfc0944
Doranbek <67508373+Doranbek@users.noreply.github.com>
2020-08-05 14:30:40 +0600

    Update FileInputOutput.cs

--- CLI/FileInputOutput.cs
+++ CLI/FileInputOutput.cs
@@ -5,5 +5,18 @@ namespace CLI
 {
     class FileInputOutput : IInputOutput
     {
+        public string path = @"C:\IO.txt";
+        public void Write(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true))
+                sw.WriteLine(text);
+
+        }
+        public string Read()
+        {
+            using (StreamReader sr = new StreamReader(path, true))
+                return sr.ReadLine();
+        }
+
     }
 }
